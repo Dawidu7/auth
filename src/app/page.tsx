8da83fec87af2068ddb6ce82714e210890563e72
Dawidu <dawidtomalik7@gmail.com>
2025-05-16 19:09:54 +0200

    Drizzle Schema Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Home</div>
+import { db } from "~/server/db"
+
+export default async function Home() {
+  const profiles = await db.query.profile.findMany()
+
+  return (
+    <div>
+      {profiles.map(({ id }) => (
+        <span key={id}>{id}</span>
+      ))}
+    </div>
+  )
 }
